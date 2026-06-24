093e0cf110ce8504f2a4329ab9a12c2e05dc472c
bugkiller001 <1516955019@qq.com>
2026-06-24 17:26:37 +0800

    Set base URL for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base:'/https://github.com/bugkiller001/Wuhan-Website/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
